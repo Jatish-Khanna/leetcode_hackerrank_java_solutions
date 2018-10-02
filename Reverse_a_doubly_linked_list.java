b8167bfaab033d3bf38bc380990f04668bffaacb
Jatish Khanna <jatish.khanna@gmail.com>
2018-10-02 10:02:45 +0200

    Reverse a doubly linked list
    You’re given the pointer to the head node of a doubly linked list. Reverse the order of the nodes in the list. The head node might be NULL to indicate that the list is empty. Change the next and prev pointers of all the nodes so that the direction of the list is reversed. Return a reference to the head node of the reversed list.
    
    Function Description
    
    Complete the reverse function in the editor below. It should return a reference to the head of your reversed list.
    
    reverse has the following parameter(s):
    
    head: a reference to the head of a DoublyLinkedList
    Input Format
    
    The first line contains an integer , the number of test cases.
    
    Each test case is of the following format:
    
    The first line contains an integer , the number of elements in the linked list.
    The next  lines contain an integer each denoting an element of the linked list.

---
+++ Reverse_a_doubly_linked_list.java
@@ -0,0 +1,113 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Solution {
+
+	static class DoublyLinkedListNode {
+		public int data;
+		public DoublyLinkedListNode next;
+		public DoublyLinkedListNode prev;
+
+		public DoublyLinkedListNode(int nodeData) {
+			this.data = nodeData;
+			this.next = null;
+			this.prev = null;
+		}
+	}
+
+	static class DoublyLinkedList {
+		public DoublyLinkedListNode head;
+		public DoublyLinkedListNode tail;
+
+		public DoublyLinkedList() {
+			this.head = null;
+			this.tail = null;
+		}
+
+		public void insertNode(int nodeData) {
+			DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+			if (this.head == null) {
+				this.head = node;
+			} else {
+				this.tail.next = node;
+				node.prev = this.tail;
+			}
+
+			this.tail = node;
+		}
+	}
+
+	public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
+			throws IOException {
+		while (node != null) {
+			bufferedWriter.write(String.valueOf(node.data));
+
+			node = node.next;
+
+			if (node != null) {
+				bufferedWriter.write(sep);
+			}
+		}
+	}
+	// Complete the reverse function below.
+
+	/*
+	 * For your reference:
+	 *
+	 * DoublyLinkedListNode { int data; DoublyLinkedListNode next;
+	 * DoublyLinkedListNode prev; }
+	 *
+	 */
+	static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		DoublyLinkedListNode newHead = head;
+		DoublyLinkedListNode currentNode = head;
+		while (currentNode != null) {
+			DoublyLinkedListNode previousNode = currentNode.prev;
+			currentNode.prev = currentNode.next; // <-A<-B to A=>B [prev, next]
+			currentNode.next = previousNode; // A=>B to NULL<-A->B
+			newHead = currentNode; // New head pointer NULL<-(A)<-B
+			currentNode = currentNode.prev; // Current node NULL<-A<-[B]
+
+		}
+		return newHead;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		// BufferedWriter bufferedWriter = new BufferedWriter(new
+		// FileWriter(System.getenv("OUTPUT_PATH")));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int tItr = 0; tItr < t; tItr++) {
+			DoublyLinkedList llist = new DoublyLinkedList();
+
+			int llistCount = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < llistCount; i++) {
+				int llistItem = scanner.nextInt();
+				scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+				llist.insertNode(llistItem);
+			}
+
+			DoublyLinkedListNode llist1 = reverse(llist.head);
+
+			printDoublyLinkedList(llist1, " ", bufferedWriter);
+			bufferedWriter.newLine();
+		}
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
+}
