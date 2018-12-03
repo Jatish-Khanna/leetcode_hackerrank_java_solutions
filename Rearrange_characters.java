285d5d4eb19e8f691a60045ddd1891bb493b1b74
Jatish Khanna <jatish.khanna@gmail.com>
2018-12-03 16:32:08 +0100

    Create Rearrange_characters.java
    Rearrange characters in a string such that no two adjacent are same
    Given a string with repeated characters, task is rearrange characters in a string so that no two adjacent characters are same.
    
    Note : It may be assumed that the string has only lowercase English alphabets.
    
    Examples:
    
    Input: aaabc 
    Output: abaca 
    
    Input: aaabb
    Output: ababa 
    
    Input: aa 
    Output: Not Possible
    
    Input: aaaabc 
    Output: Not Possible
    Asked In : Amazon Interview
    Recommended: Please solve it on “PRACTICE” first, before moving on to the solution.
    
    
     
    
    Prerequisite : priority_queue.
    
    The idea is to put highest frequency character first (a greedy approach). We use a priority queue (Or Binary Max Heap) and put all characters and ordered by their frequencies (highest frequency character at root). We one by one take highest frequency character from the heap and add it to result. After we add, we decrease frequency of the character and we temporarily move this character out of priority queue so that it is not picked next time.
    
    We have to follow the step to solve this problem, they are:
    1. Build a Priority_queue or max_heap, pq that stores characters and their frequencies.
    …… Priority_queue or max_heap is built on the bases of frequency of character.
    2. Create a temporary Key that will used as the previous visited element ( previous element in resultant string. Initialize it { char = ‘#’ , freq = ‘-1’ }
    3. While pq is not empty.
    ….. Pop an element and add it to result.
    ….. Decrease frequency of the popped element by ‘1’
    ….. Push the previous element back into the priority_queue if it’s frequency > ‘0’
    ….. Make the current element as previous element for the next iteration.
    4. If length of resultant string and original, print “not possible”. Else print result.

---
+++ Rearrange_characters.java
@@ -0,0 +1,56 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+
+	static class Entry {
+		char key;
+		int freq;
+
+		Entry(char key, int freq) {
+			this.key = key;
+			this.freq = freq;
+		}
+
+	}
+
+	private static void rearrangeStringNoSameAlternateCharacter(String str) {
+		PriorityQueue<Entry> freqQueue = new PriorityQueue<Entry>(new Comparator<Entry>() {
+
+			@Override
+			public int compare(Entry e1, Entry e2) {
+				return e2.freq - e1.freq;
+			}
+
+		});
+
+		int[] freqMap = new int[256];
+		for (int index = 0; index < str.length(); index++) {
+			freqMap[str.charAt(index)]++;
+		}
+
+		for (int index = 0; index < 256; index++) {
+			if (freqMap[index] > 0)
+				freqQueue.offer(new Entry((char) index, freqMap[index]));
+		}
+
+		StringBuilder sb = new StringBuilder();
+		Entry prev = new Entry('#', -1);
+		while (!freqQueue.isEmpty()) {
+			Entry entry = freqQueue.poll();
+			sb.append(entry.key);
+			if (prev.freq > 0)
+				freqQueue.offer(prev);
+			entry.freq--;
+			prev = entry;
+		}
+		System.out.println(sb.length() == str.length() ? sb.toString() : "Not possible");
+	}
+
+	// Driver program to test above function
+	public static void main(String[] args) {
+		String str = "bbba";
+		rearrangeStringNoSameAlternateCharacter(str);
+	}
+
+}
