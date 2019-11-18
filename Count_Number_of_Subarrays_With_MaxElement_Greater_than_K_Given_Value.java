25406511d4571ef2bb7edde883031339c60dff7f
Jatish Khanna <jatish.khanna@gmail.com>
2019-11-18 14:30:53 +0530

    Update Count_Number_of_Subarrays_With_MaxElement_Greater_than_K_Given_Value.java

--- Count_Number_of_Subarrays_With_MaxElement_Greater_than_K_Given_Value.java
+++ Count_Number_of_Subarrays_With_MaxElement_Greater_than_K_Given_Value.java
@@ -1,20 +1,18 @@
 
 public class Solution {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     int[] arr = {3, 4, 2, 1, 5};
-    int lowerBoundary = 2;
+    int lowerBoundary = 3;
 
     long totalSubarrays = countSubarrays(arr.length);
     int length = 0;
-    int subIndex = 0;
     long subarraysWithLowerValue = 0;
     for (int index = 0; index < arr.length; index++) {
-      for (subIndex = index; subIndex < arr.length && arr[subIndex] <= lowerBoundary; subIndex++) {
+      length = 0;
+      for (; index < arr.length && arr[index] <= lowerBoundary; index++) {
         length++;
       }
-      index = subIndex;
       subarraysWithLowerValue += countSubarrays(length);
-      length = 0;
     }
 
     System.out.println(totalSubarrays - subarraysWithLowerValue);
