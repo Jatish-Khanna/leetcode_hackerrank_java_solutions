507bffdbdea8a5aeba2a8ad6ac64ed81fe31ad37
Jatish Khanna <jatish.khanna@gmail.com>
2019-12-17 17:45:45 +0530

    Update 922._Sort_Array_By_Parity_II.java

--- leetcode/922._Sort_Array_By_Parity_II.java
+++ leetcode/922._Sort_Array_By_Parity_II.java
@@ -1,6 +1,6 @@
 public class Solution {
 
-    public int[] sortArrayByParityII(int[] A) {
+    public int[] sortArrayByParityIIDoublePointer(int[] A) {
         int oddIndex = 1;
         int evenIndex = 0;
         while(evenIndex < A.length && oddIndex < A.length) {
@@ -13,6 +13,21 @@ public class Solution {
         }
         return A;
     }
+    
+    public int[] sortArrayByParityII(int[] A) {
+        int oddIndex = 1;
+        for(int index = 0; index < A.length; index += 2) {
+            if(A[index] % 2 == 1) {
+                while(oddIndex < A.length && A[oddIndex] % 2 == 1) {
+                    oddIndex += 2;
+                }
+                swap(A, index, oddIndex);
+                oddIndex += 2;
+            }
+        }
+        return A;
+    }
+    
     public void swap(int []A, int index, int oddIndex) {
         int temp = A[index];
         A[index] = A[oddIndex];
