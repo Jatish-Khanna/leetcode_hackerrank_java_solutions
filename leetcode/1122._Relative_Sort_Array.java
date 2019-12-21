5c3846a0d26857f60152f92b2dd535552fdabd60
Jatish Khanna <jatish.khanna@gmail.com>
2019-12-21 19:12:05 +0530

    Update 1122._Relative_Sort_Array.java

--- leetcode/1122._Relative_Sort_Array.java
+++ leetcode/1122._Relative_Sort_Array.java
@@ -29,4 +29,34 @@ public class Solution {
         }
         return result;
     }
+    
+    // 60% faster than using Collection framework elements
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+       int []freq = new int[1001];
+       
+        for(int element : arr1) {
+
+            freq[element]++;
+        }
+        
+        int []result = new int[arr1.length];
+        int index = 0;
+        
+        for(int element : arr2) {
+
+            while(freq[element]-- > 0) {
+                result[index++] = element;
+            }
+        }
+        
+        
+        for(int  f = 0; f < freq.length; f++) {
+
+            while(freq[f]-- > 0) {
+                result[index++] = f;
+            }
+        }
+        return result;
+    }
 }
+
