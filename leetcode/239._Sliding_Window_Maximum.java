04831511f67962c793828cca05e4bc66c7b46e19
Jatish Khanna <jatish.khanna@gmail.com>
2019-12-26 12:40:06 +0530

    Update 239._Sliding_Window_Maximum.java

--- leetcode/239._Sliding_Window_Maximum.java
+++ leetcode/239._Sliding_Window_Maximum.java
@@ -32,4 +32,43 @@ public class Solution {
         result[rIndex] = nums[queue.peek()];
         return result;
     }
+    
+    // The approach is O(N) where left and right arrays are used using DP solution
+    
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        
+        int size = nums.length;
+        int []result = new int[size - k + 1];
+
+        int []left = new int[size];
+        int []right = new int[size];
+        left[0] = nums[0];
+        right[size - 1] = nums[size - 1];
+        
+        for(int index = 1; index < size; index++) {
+            if(index % k == 0) {
+                left[index] = nums[index];
+            } else {
+                left[index] = Math.max(left[index - 1], nums[index]);
+            }
+            
+            int rt = size - index - 1;
+            if(rt % k == (k - 1)) {
+                right[rt] =  nums[rt];
+            } else {
+                right[rt] = Math.max(right[rt + 1], nums[rt]);
+            }
+            
+        }
+        
+        for (int i = 0, j = i + k - 1; j < size; ++i, ++j) {
+            result[i] = Math.max(left[j], right[i]);
+        }
+        
+        
+        return result;
+    }
 }
