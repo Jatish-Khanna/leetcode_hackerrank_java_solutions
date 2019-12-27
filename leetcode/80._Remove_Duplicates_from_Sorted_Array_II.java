4b33afa0ceb9908751fe468f91a9be39d2039247
Jatish Khanna <jatish.khanna@gmail.com>
2019-12-27 10:40:43 +0530

    Update 80._Remove_Duplicates_from_Sorted_Array_II.java

--- leetcode/80._Remove_Duplicates_from_Sorted_Array_II.java
+++ leetcode/80._Remove_Duplicates_from_Sorted_Array_II.java
@@ -45,4 +45,25 @@ public class Solution {
         }
         return total;
     }
+    
+    // Tortoise and hare approach is best and fastest of all
+    public int findDuplicate(int[] nums) {
+        
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
 }
