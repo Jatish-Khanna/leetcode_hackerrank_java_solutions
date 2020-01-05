e8bfb439f922e2c44697714a131e112aa010027b
Jatish Khanna <jatish.khanna@gmail.com>
2020-01-05 08:56:00 +0530

    Update 137._Single_Number_II.java
    Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
    
    Note:
    
    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
    
    Example 1:
    
    Input: [2,2,3,2]
    Output: 3
    Example 2:
    
    Input: [0,1,0,1,0,1,99]
    Output: 99

--- leetcode/137._Single_Number_II.java
+++ leetcode/137._Single_Number_II.java
@@ -2,3 +2,13 @@
 
 public class Solution {
 
+        public int singleNumber(int[] nums) {
+        int one = 0;
+        int two = 0;
+        for ( int i = 0 ; i < nums.length; i++){
+            one = (one^nums[i])&~two;
+            two = (two^nums[i])&~one;
+        }
+        return one;
+    }
+}
