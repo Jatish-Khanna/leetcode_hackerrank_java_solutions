e0d31a9cfbe938f5aaaf2c9fc609922030174660
Jatish Khanna <jatish.khanna@gmail.com>
2020-01-09 10:44:25 +0530

    Create 33._Search_in_Rotated_Sorted_Array.java
    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
    
    (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
    
    You are given a target value to search. If found in the array return its index, otherwise return -1.
    
    You may assume no duplicate exists in the array.
    
    Your algorithm's runtime complexity must be in the order of O(log n).
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1

---
+++ leetcode/33._Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,30 @@
+
+public class Solution {
+
+
+    public int search(int[] nums, int target) {
+        int result = -1;
+        if(nums.length == 0) { return result; }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while(start <= end){
+            mid = start + (end - start) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[start] <= nums[mid]) {
+               if(nums[start] <= target && target <= nums[mid]) {
+                   end = mid - 1;
+               } else {
+                   start = mid + 1;
+               }
+            } else {
+                if(nums[mid] <= target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid -1;
+                }
+            }
+        }
+        return result;
+    }
+}
