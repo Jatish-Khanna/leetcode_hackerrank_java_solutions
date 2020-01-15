57c9e31f195aae8bd302547cd60c5829f1245f4a
Jatish Khanna <jatish.khanna@gmail.com>
2020-01-15 21:17:51 +0530

    Update 451._Sort_Characters_By_Frequency.java

--- leetcode/451._Sort_Characters_By_Frequency.java
+++ leetcode/451._Sort_Characters_By_Frequency.java
@@ -1,6 +1,42 @@
 
 
 public class Solution {
+ 
+ // Fastest of All without any collections
+ 
+    public String frequencySort(String s) {
+        int []map = new int[256];
+        
+        //List<Character> list = new ArrayList<>();
+        for(char ch : s.toCharArray()) {
+           // if(map[ch] == 0) { list.add(ch); }
+           map[ch]++;
+            
+        }
+        // Collections.sort(list, (c1, c2) -> {
+        //    return map[c2] - map[c1];  
+        // });
+        
+        StringBuilder sb = new StringBuilder();
+        int maxIndex;
+        
+        while(true) {
+            maxIndex = 0;
+            for(int cIndex = 1; cIndex < map.length; cIndex++) {
+                if(map[maxIndex] < map[cIndex]) {
+                    maxIndex = cIndex;
+                }
+            }
+            
+            if(map[maxIndex] == 0) { break; }
+            
+            for(int index = 0; index < map[maxIndex]; index++) {
+                sb.append((char)maxIndex);
+            }
+            map[maxIndex] = 0;
+        }
+        return sb.toString();
+    }
 
  // Optimized by limiting space to Array and primitive types
  public String frequencySort(String s) {
