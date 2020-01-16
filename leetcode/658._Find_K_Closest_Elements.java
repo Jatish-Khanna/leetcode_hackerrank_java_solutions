3e713e421bf65b86f5ab7a074e755ccd818e2c02
Jatish Khanna <jatish.khanna@gmail.com>
2020-01-16 07:37:23 +0530

    Create 658._Find_K_Closest_Elements.java
    Given a sorted array, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
    
    Example 1:
    Input: [1,2,3,4,5], k=4, x=3
    Output: [1,2,3,4]
    Example 2:
    Input: [1,2,3,4,5], k=4, x=-1
    Output: [1,2,3,4]
    Note:
    The value k is positive and will always be smaller than the length of the sorted array.
    Length of the given array is positive and will not exceed 104
    Absolute value of elements in the array and x will not exceed 104

---
+++ leetcode/658._Find_K_Closest_Elements.java
@@ -0,0 +1,40 @@
+
+
+public class Solution {
+
+   // With Arrays.copyOfRange and Arrays.binarySearch implementation, 50% slower than other methods
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> list = new ArrayList<>();
+        if(k == 0)  {return list; }
+        
+        int index = Arrays.binarySearch(arr, x);
+        if(index == -1) { return toList(Arrays.copyOfRange(arr, 0, k)); }
+        if(-index == arr.length + 1) { return toList(Arrays.copyOfRange(arr,arr.length - k, arr.length)); }
+        
+        //System.out.println(-index);
+        int left = Math.abs(index);
+        int right = Math.abs(index + 1);
+        
+        while(right - left < k) {
+            if(left == 0) {
+                ++right;
+                continue;
+            } else if(right == arr.length) {
+                --left;
+                continue;
+            }
+            if(Math.abs(arr[left - 1] - x) <= Math.abs(arr[right] - x)) { --left; }
+            else { ++right; }
+        }
+        return toList(Arrays.copyOfRange(arr, left, right));
+    }
+    
+    public List<Integer> toList(int []arr) {
+        List<Integer> result = new ArrayList<>();
+        for(int element :arr) {
+            result.add(element);
+        }
+        return result;
+    }
+}
