e2b18e574d4e9f79ce17851b17a3c602397a3c14
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-09 09:30:11 +0530

    Update 701._Insert_into_a_Binary_Search_Tree.java

--- leetcode/701._Insert_into_a_Binary_Search_Tree.java
+++ leetcode/701._Insert_into_a_Binary_Search_Tree.java
@@ -2,6 +2,35 @@
 
 public class Solution {
 
+    // recursive
+    
+    
+    // recursive 
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+       if(root == null) { return new TreeNode(val); }
+        insert(root, val);
+       return root;
+    }
+    
+    public void insert(TreeNode root, int val) {
+        if(val < root.val) {
+           if(root.left == null) {
+               root.left = new TreeNode(val);
+               return;
+           }
+           insertIntoBST(root.left, val);
+       } else {
+           if(root.right == null) {
+               root.right = new TreeNode(val);
+               return;
+           }
+           insertIntoBST(root.right, val);
+       }
+       return;
+    }
+    
+    
+    
 // Iterative - variable
     public TreeNode insertIntoBST(TreeNode root, int val) {
        if(root == null) { return new TreeNode(val); }
