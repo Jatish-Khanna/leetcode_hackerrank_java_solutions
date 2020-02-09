8b815b4d8c81b0c0763967428951def422565de5
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-09 09:30:25 +0530

    Update 701._Insert_into_a_Binary_Search_Tree.java

--- leetcode/701._Insert_into_a_Binary_Search_Tree.java
+++ leetcode/701._Insert_into_a_Binary_Search_Tree.java
@@ -3,6 +3,15 @@
 public class Solution {
 
     // recursive
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+       if(root == null) { return new TreeNode(val); }
+       if(val < root.val) {
+           root.left = insertIntoBST(root.left, val);
+       } else {
+           root.right = insertIntoBST(root.right, val);
+       }
+       return root;
+    }
     
     
     // recursive 
