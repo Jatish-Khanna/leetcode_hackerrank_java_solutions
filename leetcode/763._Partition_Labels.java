3529ac3febb5b93707c2a94090c94d8d2c0314a1
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-09 18:52:50 +0530

    Update 763._Partition_Labels.java

--- leetcode/763._Partition_Labels.java
+++ leetcode/763._Partition_Labels.java
@@ -2,6 +2,35 @@
 
 public class Solution {
 
+    // Optimized solution 
+    
+    public List<Integer> partitionLabels(String S) {
+        int []rs = new int[26];
+        
+        // Get the rightmost position
+        for(int index = 0; index < S.length(); index++) {
+            rs[S.charAt(index) - 'a'] = index;
+        }
+        
+        int maxRight = 0;
+        int current = 0;
+        List<Integer> result = new ArrayList<>();
+        
+        for(int index = 0; index < S.length(); index++) {
+            // Number of chars in current label
+            current = index;
+            maxRight = rs[S.charAt(index) - 'a'];
+            while(current < maxRight) {
+                maxRight = Math.max(rs[S.charAt(current) - 'a'], maxRight);
+                current++;
+            } 
+            
+            result.add(maxRight - index + 1); // Length
+            index = maxRight;
+        }
+        return result;
+    }
+
 
     public List<Integer> partitionLabels(String S) {
         int []rs = new int[26];
