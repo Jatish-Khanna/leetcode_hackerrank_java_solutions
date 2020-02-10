3835c1e7eaaf7660abf356c1be1f723244ff18d6
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-10 11:18:02 +0530

    Create 415._Add_Strings.java
    Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
    
    Note:
    
    The length of both num1 and num2 is < 5100.
    Both num1 and num2 contains only digits 0-9.
    Both num1 and num2 does not contain any leading zero.
    You must not use any built-in BigInteger library or convert the inputs to integer directly.

---
+++ leetcode/415._Add_Strings.java
@@ -0,0 +1,44 @@
+
+
+public class Solution {
+
+  // Not using additional references - equal space and memory utilization
+    public String addStrings(String num1, String num2) {
+        //char []first = num1.toCharArray();
+        //char []second = num2.toCharArray();
+        int fI = num1.length() - 1;
+        int sI = num2.length() - 1;
+        
+        StringBuilder result = new StringBuilder();
+        int sum = 0;
+        int carry = 0;
+        while(fI >= 0 && sI >= 0) {
+            sum = carry + num1.charAt(fI) - 48 + num2.charAt(sI) - 48;
+            carry = sum / 10;
+            sum %= 10;
+            result.append(sum);
+            fI--;
+            sI--;
+        }
+        while(fI >= 0 ) {
+            sum = carry + num1.charAt(fI) - 48;
+            carry = sum / 10;
+            sum %= 10;
+            result.append(sum);
+            fI--;
+        }
+        
+        while(sI >= 0 ) {
+            sum = carry + num2.charAt(sI) - 48;
+            carry = sum / 10;
+            sum %= 10;
+            result.append(sum);
+            sI--;
+        }
+        if(carry > 0) {
+            result.append(carry);
+        }
+        
+        return result.length() == 0 ? "0" : result.reverse().toString();
+    }
+}
