ff9eab337b704997ca9dbae3662aeed28f3abb85
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-10 15:16:39 +0530

    Update 1019._Next_Greater_Node_In_Linked_List.java

--- leetcode/1019._Next_Greater_Node_In_Linked_List.java
+++ leetcode/1019._Next_Greater_Node_In_Linked_List.java
@@ -1,6 +1,44 @@
 
 
 public class Solution {
+  
+  static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+      this.val = val;
+    }
+  }
+  
+  // Optimized with secondary memory
+  
+    public int[] nextLargerNodes(ListNode head) {
+        int count = 0;
+        ListNode current = head;
+        while(current != null) {
+            current = current.next;
+            count++;
+        }
+        
+        int []result = new int[count];
+        
+        current = head;
+        Stack<int[]> stack = new Stack<>();
+        int index = 0;
+        
+        while(current != null) {
+            // Pop all the values already seen which are smaller than the current node
+            while(!stack.isEmpty() && current.val > stack.peek()[1]) {
+                result[stack.pop()[0]] = current.val;
+            }
+            
+            stack.push(new int[]{index, current.val});
+            index++;
+            current = current.next;
+        }
+        return result;
+    }
+
 
   // Slow solution no memorization for already found result
     public int[] nextLargerNodes(ListNode head) {
