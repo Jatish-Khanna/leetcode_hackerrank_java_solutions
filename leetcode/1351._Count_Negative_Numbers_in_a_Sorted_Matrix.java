6a2a717bb21dcfaed6cb4fc7c7482038aaa7811a
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-21 16:50:11 +0530

    Update 1351._Count_Negative_Numbers_in_a_Sorted_Matrix.java

--- leetcode/1351._Count_Negative_Numbers_in_a_Sorted_Matrix.java
+++ leetcode/1351._Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -1,6 +1,29 @@
 
 
 public class Solution {
+    
+    // clear optimization should be Binary search algo.
+    
+    
+    
+    // slight better solution - search learnly from backwards
+    
+    
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for(int rid = grid.length - 1; rid >= 0; rid--) {
+            for(int cid = grid[0].length - 1; cid >= 0; cid--) {
+                if(grid[rid][cid] >= 0) {
+                    break;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
+    
+    // Non optimized solution - as the numbers are sorted should use binary search
     public int countNegatives(int[][] grid) {
         int count = 0;
         for(int rid = 0; rid < grid.length; rid++) {
