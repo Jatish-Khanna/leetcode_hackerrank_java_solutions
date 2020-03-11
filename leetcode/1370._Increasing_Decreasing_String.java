56117b79124a6b507b991ed070755461704323b6
Jatish Khanna <jatish.khanna@gmail.com>
2020-03-11 15:46:21 +0530

    Update 1370._Increasing_Decreasing_String.java

--- leetcode/1370._Increasing_Decreasing_String.java
+++ leetcode/1370._Increasing_Decreasing_String.java
@@ -39,4 +39,43 @@ public class Solution {
             }
         return ids.toString();
         }
+    
+    
+    // Slower when using String
+
+    public String sortString(String s) {
+       int []ar = new int[26];
+        for(char ch : s.toCharArray()) {
+            ar[ch - 'a']++;
+        }
+        
+        String ids = new String();
+        int index = 0;
+        int count = 0;
+        while(count < s.length()) {
+            index = 0;
+            while(index < ar.length && count < s.length()) {
+                
+                if(ar[index] > 0) {
+                    ar[index]--;
+                    ids += ((char)(index + 'a'));
+                    count++;
+                }
+                
+                index++;
+                
+            }
+            
+            while(--index >= 0 && count < s.length()) {
+                if(ar[index] > 0) {
+                    ar[index]--;
+                    ids += ((char)(index + 'a'));
+                    count++;
+                }
+                //--index;
+                }
+            }
+        return ids.toString();
+        }
+    
     }
