496a106f406d0c046426e043e6580bfc42053488
Jatish Khanna <jatish.khanna@gmail.com>
2020-03-12 15:41:30 +0530

    Update 1374._Generate_a_String_With_Characters_That_Have_Odd_Counts.java

--- leetcode/1374._Generate_a_String_With_Characters_That_Have_Odd_Counts.java
+++ leetcode/1374._Generate_a_String_With_Characters_That_Have_Odd_Counts.java
@@ -1,6 +1,24 @@
 
 
 public class Solution {
+    
+    // Faster solution in terms of CPU cycles, dealing with the indexes
+     public String generateTheString(int n) {
+        //StringBuilder sb = new StringBuilder();
+        char []ar = new char[n];
+        
+        while(n > 1) {
+            ar[--n] = 'a';
+        }
+        if(ar.length % 2 == 0) {
+            ar[--n] = 'b';
+        } else {
+            ar[--n] = 'a';
+        }
+        return new String(ar);
+    }
+    
+    // Slower solution in terms of CPU cycles
 
     public String generateTheString(int n) {
         StringBuilder sb = new StringBuilder();
