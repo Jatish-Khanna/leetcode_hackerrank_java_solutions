86fdb6f98ba7258e4ad24d9d4b0c232a7db6cac0
Jatish Khanna <jatish.khanna@gmail.com>
2020-03-20 11:46:41 +0530

    Update 300._Longest_Increasing_Subsequence.java

--- leetcode/300._Longest_Increasing_Subsequence.java
+++ leetcode/300._Longest_Increasing_Subsequence.java
@@ -2,6 +2,27 @@
 
 public class Solution {
 
+    // Commented Arrays.fill
+        public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) { return 0; }
+        int []length = new int[nums.length];
+        int max = 1;
+        //Arrays.fill(length, 1);
+        length[0] = 1;
+        for(int index = 1; index < nums.length; index++) {
+            length[index] = 1;
+            for(int current = 0; current < index; current++) {
+                if(nums[current] < nums[index] && length[current] + 1 > length[index]) {
+                    length[index] = length[current] + 1;
+                }
+            }
+            max = Math.max(max, length[index]);
+        }
+        
+        return max;
+    }
+    
+    
     public int lengthOfLIS(int[] nums) {
         if(nums.length == 0) { return 0; }
         int []length = new int[nums.length];
