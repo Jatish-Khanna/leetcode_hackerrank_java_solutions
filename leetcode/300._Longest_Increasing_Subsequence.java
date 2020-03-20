7d8010d72e9f7e2257d2f3584c1003a013dd1d81
Jatish Khanna <jatish.khanna@gmail.com>
2020-03-20 12:27:48 +0530

    Update 300._Longest_Increasing_Subsequence.java

--- leetcode/300._Longest_Increasing_Subsequence.java
+++ leetcode/300._Longest_Increasing_Subsequence.java
@@ -1,6 +1,43 @@
 
 
 public class Solution {
+    
+    // Binary search O(n logn)
+        public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) { return 0; }
+        
+        int []element = new int[nums.length];
+        int max = 1;
+        element[0] = nums[0];
+        int last;
+        for(int index = 1; index < nums.length; index++) {
+            last = binarySearch(element, 0, max, nums[index]);
+            
+            if(last >= max) {
+                element[max++] = nums[index];
+            } else {
+                element[last] = nums[index];
+            }
+        }
+        return max;
+    }
+    
+    private int binarySearch(int []element, int start, int end, int target) {
+        int mid;
+        //element[end] = -1;
+        while(start < end) {
+            mid = start + (end - start) / 2;
+            if(element[mid] == target) {
+                return mid;
+            } else if(element[mid] > target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
 
     // Commented Arrays.fill
         public int lengthOfLIS(int[] nums) {
