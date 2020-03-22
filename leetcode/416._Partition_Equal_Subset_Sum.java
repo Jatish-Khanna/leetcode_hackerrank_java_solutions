bee830ed2ec2e41d160fdc57bea298544dbb5ceb
Jatish Khanna <jatish.khanna@gmail.com>
2020-03-22 07:52:31 +0530

    Update 416._Partition_Equal_Subset_Sum.java

--- leetcode/416._Partition_Equal_Subset_Sum.java
+++ leetcode/416._Partition_Equal_Subset_Sum.java
@@ -18,4 +18,28 @@ public class Solution {
         if(nums[index] > target) { return false; }
         return isSubsetPossible(index - 1, target - nums[index], nums) || isSubsetPossible(index - 1, target, nums);
     }
+    
+    // Slow solution in terms of Memory and CPU cycle when compared to recursive approach
+        public boolean canPartition(int[] nums) {
+        int total = 0;
+        for(int num : nums) {
+           total += num;
+        }
+        
+        if(total % 2 != 0) { return false; }
+        total = total / 2;
+       
+        boolean [][]dpSol = new boolean[total + 1][nums.length + 1];
+        Arrays.fill(dpSol[0], true);
+        
+        for(int sum = 1; sum <= total; sum++) {
+            for(int element = 1; element <= nums.length; element++) {
+                dpSol[sum][element] = dpSol[sum][element - 1];
+                if(sum >= nums[element - 1]) {
+                    dpSol[sum][element] = dpSol[sum][element -1 ] || dpSol[sum - nums[element - 1]][element - 1];
+                }
+            }
+        }
+        return dpSol[total][nums.length];
+    }
 }
