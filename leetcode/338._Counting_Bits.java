d66035a70171b5ecd180b585fefc6d52e77893ac
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-14 14:38:34 +0530

    Update 338._Counting_Bits.java

--- leetcode/338._Counting_Bits.java
+++ leetcode/338._Counting_Bits.java
@@ -1,7 +1,27 @@
 
 
 public class Solution {
+  
+    // Faster as the solution suggests - a simple dynamic programming approach
+    
+    public int[] countBits(int num) {
+        int[] ar = new int[num+1];
+        if(num == 0) {
+            return ar;
+        }
+        
+        ar[1] = 1;
+        for(int index = 1; index <= num; index++) {
+            if(index % 2 != 0) {
+                ar[index] = 1;
+            } 
+            ar[index] += ar[index/2];
+        }
+        return ar;
+    }
 
+    
+    // Slower solution
 
     public int[] countBits(int num) {
         int[] ar = new int[num+1];
