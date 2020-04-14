c539e2cc9427b9a84a36f1792f457529c2471e68
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-14 15:32:21 +0530

    Update 1323._Maximum_69_Number.java

--- leetcode/1323._Maximum_69_Number.java
+++ leetcode/1323._Maximum_69_Number.java
@@ -1,6 +1,19 @@
 
 
 public class Solution {
+    
+    // Faster method without integer to string conversion
+    
+    public int maximum69Number (int num) {
+        int sum = 3;
+        for(int div = 1000; div > 0; div = div/10) {
+            if((num / div)%10 == 6) {
+                return num + sum * div;
+            }
+        }
+        return num;
+    }
+
 
     public int maximum69Number (int num) {
         String number = String.valueOf(num);
