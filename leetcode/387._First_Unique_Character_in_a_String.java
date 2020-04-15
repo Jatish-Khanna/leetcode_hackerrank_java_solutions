246a107d91089760bccae1f67a0861f146edf064
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-15 09:49:06 +0530

    Update 387._First_Unique_Character_in_a_String.java

--- leetcode/387._First_Unique_Character_in_a_String.java
+++ leetcode/387._First_Unique_Character_in_a_String.java
@@ -1,6 +1,33 @@
 
 
 public class Solution {
+    
+    // Little optimization 
+    
+    public int firstUniqChar(String s) {
+        int []ar = new int[26];
+        //char []ch = ;
+        
+        int index = 1;
+        int id;
+        int max = s.length() + 1;
+        for(char val : s.toCharArray()) {
+            id = val - 'a';
+            if(ar[id] != max && ar[id] > 0) {
+                ar[id] = max;
+            } else if(ar[id] == 0){
+                ar[id] = index;
+            }
+            index++;
+        }
+        
+        int min = max;
+        for(int i = 0; i < 26; i++) {
+            if(ar[i] > 0 && min > ar[i])
+              min = ar[i];
+        }
+        return min == max ? -1 : min - 1;
+    }
 
     public int firstUniqChar(String s) {
         int []ar = new int[26];
