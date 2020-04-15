ba2f5870767c6c160c079198783f08f76a6c8561
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-15 15:13:29 +0530

    Update 453._Minimum_Moves_to_Equal_Array_Elements.java

--- leetcode/453._Minimum_Moves_to_Equal_Array_Elements.java
+++ leetcode/453._Minimum_Moves_to_Equal_Array_Elements.java
@@ -1,4 +1,16 @@
 class Solution {
+    
+    /*
+     Based on the formulae;
+     where n = number of elements
+     sum = total of all the elements without modification
+     m =  number of moves
+     
+     a = (minElement + m) // means minElement will be modified at least m times to get sum =  n . a
+     
+     sum + m * (n - 1) = a * n
+    */
+    
     public int minMoves(int[] nums) {
         int min = Integer.MAX_VALUE;
         int sum = 0;
