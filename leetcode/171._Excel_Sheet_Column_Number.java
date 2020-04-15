a2c4c248ce6acc0ee9570a1969bee3ae2b220563
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-15 13:32:54 +0530

    Create 171._Excel_Sheet_Column_Number.java
    Given a column title as appear in an Excel sheet, return its corresponding column number.
    
    For example:
    
        A -> 1
        B -> 2
        C -> 3
        ...
        Z -> 26
        AA -> 27
        AB -> 28 
        ...
    Example 1:
    
    Input: "A"
    Output: 1
    Example 2:
    
    Input: "AB"
    Output: 28
    Example 3:
    
    Input: "ZY"
    Output: 701

---
+++ leetcode/171._Excel_Sheet_Column_Number.java
@@ -0,0 +1,12 @@
+
+
+public class Solution {
+
+    public int titleToNumber(String s) {
+        int result = 0;
+        for(char ch : s.toCharArray()) {
+            result = result * 26 + (ch - 'A' + 1);
+        }
+        return result;
+    }
+}
