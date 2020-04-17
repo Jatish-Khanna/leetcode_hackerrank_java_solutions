0e73b107f94f6990012e51de8d04f9ecaa41805c
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-17 09:07:54 +0530

    Create 914._X_of_a_Kind_in_a_Deck_of_Cards.java
    In a deck of cards, each card has an integer written on it.
    
    Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
    
    Each group has exactly X cards.
    All the cards in each group have the same integer.
     
    
    Example 1:
    
    Input: deck = [1,2,3,4,4,3,2,1]
    Output: true
    Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
    Example 2:
    
    Input: deck = [1,1,1,2,2,2,3,3]
    Output: false´
    Explanation: No possible partition.
    Example 3:
    
    Input: deck = [1]
    Output: false
    Explanation: No possible partition.
    Example 4:
    
    Input: deck = [1,1]
    Output: true
    Explanation: Possible partition [1,1].
    Example 5:
    
    Input: deck = [1,1,2,2,2,2]
    Output: true
    Explanation: Possible partition [1,1],[2,2],[2,2].
     
    
    Constraints:
    
    1 <= deck.length <= 10^4
    0 <= deck[i] < 10^4

---
+++ leetcode/914._X_of_a_Kind_in_a_Deck_of_Cards.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        int []suit = new int[10000];
+        int last = Integer.MAX_VALUE;
+        for(int card : deck) {
+            suit[card]++;
+            last = suit[card];
+        }
+
+        for(int count : suit) {
+            if(count == 1) {
+                return false;
+            } else if(count > 1) {
+                last = gcd(last, count);        
+            }
+        }
+        return last > 1;
+    }
+    
+    private int gcd(int one, int two) {
+        if(two == 0) {
+            return one;
+        }
+        return gcd(two, one % two);
+    }
+}
