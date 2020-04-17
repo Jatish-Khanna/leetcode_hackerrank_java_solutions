d7cda1ab9f39f3b4bb7dcbe5953777b91327ae9c
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-17 09:37:49 +0530

    Update 633._Sum_of_Square_Numbers.java

--- leetcode/633._Sum_of_Square_Numbers.java
+++ leetcode/633._Sum_of_Square_Numbers.java
@@ -1,4 +1,25 @@
 class Solution {
+    
+    // A bit optimized
+    public boolean judgeSquareSum(int c) {
+        int  sq = (int)Math.sqrt(c);
+        int mul;
+        for(int a = sq / 2; a <= sq;) {
+            mul = a * a;
+            if(isSquareRoot(c - (mul))) {
+                return true;
+            } else if(c - mul > 0) {
+                a++;
+            } else {
+                a--;
+            }
+        }
+        return false;
+    }
+    
+    
+    // Slower
+    
     public boolean judgeSquareSum(int c) {
         int  sq = (int)Math.sqrt(c);
         for(int a = 0; a <= sq; a++) {
