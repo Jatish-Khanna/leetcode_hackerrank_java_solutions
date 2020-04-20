85d1ec89fe5ddc6a0c67b475121cda3a6ee3aa56
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-20 10:22:27 +0530

    Update 788._Rotated_Digits.java

--- leetcode/788._Rotated_Digits.java
+++ leetcode/788._Rotated_Digits.java
@@ -1,4 +1,34 @@
 class Solution {
+    
+    // DFS based - Faster solution
+    class Solution {
+    static int []next = {0, 1, 8, 2, 5, 6, 9};
+    static int ans = 0; 
+    public int rotatedDigits(int N) {
+        if(N < 2) { return 0; }
+        ans = 0;
+        dfs(0, false, N);
+        return ans;
+    }
+    
+    private void dfs(int num, boolean isValid, int N) {
+        if(num > N) { return; }
+        if(isValid) {
+            ans++;
+        }
+        int another;
+        boolean aValid;
+        for(int i = 0; i < next.length; i++) {
+            aValid = isValid || (i > 2);
+            another = (num * 10) + next[i];
+            if(another == 0 || another > N) { continue; }
+            dfs(another, aValid, N);
+        }
+    }
+    
+    
+    // Slower solution
+    
     public int rotatedDigits(int N) {
         int curr = 1;
         int result = 0;
