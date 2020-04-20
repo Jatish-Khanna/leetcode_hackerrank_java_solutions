aba3e59901c6a1a7ce015453f6cf8fa1fed1b237
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-20 16:12:07 +0530

    Update 520._Detect_Capital.java

--- leetcode/520._Detect_Capital.java
+++ leetcode/520._Detect_Capital.java
@@ -1,4 +1,37 @@
 class Solution {
+    
+    // same speed
+    public boolean detectCapitalUse(String word) {
+        
+        if(word.length() == 1) {
+            return true;
+        }
+       
+        if(!caps(word.charAt(0)) && caps(word.charAt(1))) {
+            return false;
+        }
+        
+        if(caps(word.charAt(1))) {
+            for(int index = 2; index < word.length(); index++) {
+                if(!caps(word.charAt(index))) {
+                    return false;
+                }
+            }
+        } else {
+             for(int index = 2; index < word.length(); index++) {
+                if(caps(word.charAt(index))) {
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+    }
+   
+    
+    // same speed
+    
+    
     public boolean detectCapitalUse(String word) {
         char ch[] = word.toCharArray();
         int count = 0;
