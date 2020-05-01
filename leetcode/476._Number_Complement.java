5a98a42317764c45d648b81e2d0d9a302fb720d0
Jatish Khanna <jatish.khanna@gmail.com>
2020-05-01 13:03:57 +0530

    Update 476._Number_Complement.java

--- leetcode/476._Number_Complement.java
+++ leetcode/476._Number_Complement.java
@@ -12,4 +12,19 @@ class Solution {
         // System.out.println(Integer.toBinaryString(ones) + " " + Integer.toBinaryString(num));
         return ones ^ num;
     }
+    
+    
+    public int findComplement(int num) {
+        int n = num;
+        if(num == 0) {
+            return 1;
+        }
+        int ones = 1;
+        while(n > 0) {
+            n = n >> 1;
+            ones = ones << 1;
+        }
+        // System.out.println(Integer.toBinaryString(ones - 1) + " " + Integer.toBinaryString(num));
+        return (ones - 1) ^ num;
+    }
 }
