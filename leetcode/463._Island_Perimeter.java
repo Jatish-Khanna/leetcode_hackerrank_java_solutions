bde75b158e27e299e53ca67283498935efa57a0b
Jatish Khanna <jatish.khanna@gmail.com>
2020-05-04 10:08:45 +0530

    Update 463._Island_Perimeter.java

--- leetcode/463._Island_Perimeter.java
+++ leetcode/463._Island_Perimeter.java
@@ -1,6 +1,28 @@
 class Solution {
     // 01 10 0-1 -10
     public int islandPerimeter(int[][] grid) {
+            int perimter = 0;
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    perimter+= 4;
+                    if(i>0 && grid[i-1][j] == 1){
+                        perimter-=2;
+                    }
+                    if(j>0 && grid[i][j-1] == 1){
+                        perimter-=2;
+                    }
+                }
+            }
+        }
+        return perimter;
+    }
+
+    
+    
+    // 01 10 0-1 -10
+    public int islandPerimeter(int[][] grid) {
         int per = 0;
         int m = grid.length;
         int n = grid[0].length;
