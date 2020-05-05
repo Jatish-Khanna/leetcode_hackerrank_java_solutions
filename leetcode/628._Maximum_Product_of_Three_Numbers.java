df63a6b44aeb8168985291e1cbe781d36eea2a79
Jatish Khanna <jatish.khanna@gmail.com>
2020-05-05 08:25:30 +0530

    Update 628._Maximum_Product_of_Three_Numbers.java

--- leetcode/628._Maximum_Product_of_Three_Numbers.java
+++ leetcode/628._Maximum_Product_of_Three_Numbers.java
@@ -1,4 +1,42 @@
 class Solution {
+    
+    // Faster solution
+
+    public int maximumProduct(int[] nums) {
+        int m1 = -1002;
+        int m2 = -1002;
+        int m3 = -1002;
+        int s1 = 1001;
+        int s2 = 1001;
+        int tmp;
+        for(int num : nums) {
+            if(m3 < num) {
+                m3 = num;
+                if(m2 < num) {
+                    m3 = m2;
+                    m2 = num;
+                    if(m1 < num) {
+                        m2 = m1;
+                        m1 = num;
+                    }
+                }
+            }
+            
+            if(s2 > num) {
+                s2 = num;
+                if(s1 > num) {
+                    s2 = s1;
+                    s1 = num;
+                }
+            }
+        }
+        
+        
+        return Math.max(m1 * m2 * m3, m1 * s1 * s2);
+    }
+
+    
+    // SLower solution due to priority queue
     public int maximumProduct(int[] nums) {
         int min1 = 1001;
         int min2 = min1;
