eda9cb47057c1b3c77e13de3c09057ad3cc7c287
Jatish Khanna <jatish.khanna@gmail.com>
2020-08-29 13:43:09 +0530

    Update 15._3Sum.java

--- leetcode/15._3Sum.java
+++ leetcode/15._3Sum.java
@@ -1,7 +1,47 @@
 
 
 public class Solution {
-
+   
+    // Optimization by skipping more elements
+    public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    if(nums == null || nums.length < 3) return result;
+    Arrays.sort(nums);
+    int i = 0;
+    while(nums[i] <= 0 && i < nums.length - 1) {
+      if(nums[i] > 0) return result;
+      if(i > 0 && nums[i] == nums[i-1]) {
+        i++;
+        continue;
+    }
+      int left = i + 1;
+      int right = nums.length - 1;
+      int target = 0 - nums[i];
+      while(left < right ) {
+        if(nums[left] + nums[right] < target) left++;
+        else if(nums[left] + nums[right] > target) right--;
+        else {
+          List<Integer> tmp = new ArrayList<>();
+          tmp.add(nums[i]);
+          tmp.add(nums[left]);
+          tmp.add(nums[right]);
+          result.add(tmp);
+          while (left < right && nums[left] == nums[left + 1]) {
+            left++;
+          }
+          while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+          }
+          left++;
+          right--;
+        }
+      }
+      i++;
+    }
+    return result;
+  }
+    
+    
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
