a92cb266c575567c14770b83b466c9c803b6c0a4
Jatish Khanna <jatish.khanna@gmail.com>
2020-09-23 08:10:54 +0530

    Update 204._Count_Primes.java

--- leetcode/204._Count_Primes.java
+++ leetcode/204._Count_Primes.java
@@ -1,5 +1,30 @@
 
 public class Solution {
+    
+    // Major optimization with travering only odd numbers
+    public int countPrimes(int n) {
+      
+        
+        if (n < 3) {
+            return 0;
+        }
+        boolean[] notPrime = new boolean[n];
+        int count = n / 2;
+        int limit = (int)Math.ceil(Math.sqrt(n));
+        // Loop only through odd numbers
+        for(int i = 3; i < limit; i += 2){
+            if (!notPrime[i]) {
+                for (int j = i * i; j < n; j += 2 * i){
+                    if(!notPrime[j]){
+                        notPrime[j] = true;
+                        count--;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
 
     public int countPrimes(int n) {
       boolean []ar = new boolean[n];
