8c5db8bc9769bf9f1637605f7443c44aeff6986e
Jatish Khanna <jatish.khanna@gmail.com>
2020-10-01 08:37:21 +0530

    Update 846._Hand_of_Straights.java
    Alice has a hand of cards, given as an array of integers.
    
    Now she wants to rearrange the cards into groups so that each group is size W, and consists of W consecutive cards.
    
    Return true if and only if she can.
    
     
    
    Example 1:
    
    Input: hand = [1,2,3,6,2,3,4,7,8], W = 3
    Output: true
    Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8].
    Example 2:
    
    Input: hand = [1,2,3,4,5], W = 4
    Output: false
    Explanation: Alice's hand can't be rearranged into groups of 4.
     
    
    Constraints:
    
    1 <= hand.length <= 10000
    0 <= hand[i] <= 10^9
    1 <= W <= hand.length
    Note: This question is the same as 1296: https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
    Accepted

--- leetcode/846._Hand_of_Straights.java
+++ leetcode/846._Hand_of_Straights.java
@@ -2,6 +2,53 @@
 
 public class Solution {
 
+ // Correct Solution
+ 
+    public boolean isNStraightHand(int[] hand, int W) {
+        Arrays.sort(hand);
+        int n=hand.length;
+        
+        if(n % W!=0)return false;
+        
+        boolean[] visited = new boolean[n];
+        
+        int current = 0;
+        int next = 0;
+        int size = 0;
+        int prev = -1;
+        
+        while(current < n) {
+            if(visited[current]) {
+                current++;
+                continue;
+            }
+            size = 1;
+            visited[current] = true;
+            prev = hand[current];
+            next = current + 1;
+            
+            while(size < W && next < n) {
+                if(visited[next] || hand[next] == prev) {
+                    next++;
+                    continue;
+                } else if(prev + 1 == hand[next]) {
+                    size++;
+                    visited[next] = true;
+                    prev = hand[next];
+                    next++;
+                } else {
+                    return false;
+                }
+            }
+            if(size < W && next == n) { return false; }
+            current++;
+        }
+        
+        return true;
+    }
+
+ 
+ // Incorrect Solution
  
     public boolean isNStraightHand(int[] hand, int W) {
         int remainder = hand.length % W;
