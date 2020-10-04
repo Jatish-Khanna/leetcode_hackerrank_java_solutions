313bead061738ed555388f42cc2213de1fc49adc
Jatish Khanna <jatish.khanna@gmail.com>
2020-10-04 18:14:31 +0530

    Update 852._Peak_Index_in_a_Mountain_Array.java
    Let's call an array arr a mountain if the following properties hold:
    
    arr.length >= 3
    There exists some i with 0 < i < arr.length - 1 such that:
    arr[0] < arr[1] < ... arr[i-1] < arr[i]
    arr[i] > arr[i+1] > ... > arr[arr.length - 1]
    Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
    
     
    
    Example 1:
    
    Input: arr = [0,1,0]
    Output: 1
    Example 2:
    
    Input: arr = [0,2,1,0]
    Output: 1
    Example 3:
    
    Input: arr = [0,10,5,2]
    Output: 1
    Example 4:
    
    Input: arr = [3,4,5,1]
    Output: 2
    Example 5:
    
    Input: arr = [24,69,100,99,79,78,67,36,26,19]
    Output: 2
     
    
    Constraints:
    
    3 <= arr.length <= 104
    0 <= arr[i] <= 106
    arr is guaranteed to be a mountain array.
    Accepted

--- leetcode/852._Peak_Index_in_a_Mountain_Array.java
+++ leetcode/852._Peak_Index_in_a_Mountain_Array.java
@@ -1,6 +1,31 @@
 
 
 public class Solution {
+    
+    // another way - no optimizations
+    public int peakIndexInMountainArray(int[] A) {
+        return mountainIndex(A, 0, A.length - 1);
+    }
+    
+    public int mountainIndex(int []A, int start, int end) {
+       int mid = -1;
+        while(start <= end) {
+        mid = start + (end - start) / 2;
+            // Found the mountain
+        if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1] ){
+                return mid;
+        } else if(A[mid] > A[mid + 1]) {
+            // decreasing order at mid- mountain point to the left
+            end = mid;
+        } else {
+            // increasing order - mountain point to the right
+             start = mid;
+        }
+        }
+        return mid;
+    }
+
+    
 
     public int peakIndexInMountainArray(int[] A) {
         return mountainIndex(A, 0, A.length - 1);
