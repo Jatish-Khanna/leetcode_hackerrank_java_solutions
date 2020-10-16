9a274ac2e47e7a7aaf11a3e0ab4512091c1e177b
Jatish Khanna <jatish.khanna@gmail.com>
2020-10-16 14:18:47 +0530

    Update 658._Find_K_Closest_Elements.java
    Given a sorted array arr, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
    
     
    
    Example 1:
    
    Input: arr = [1,2,3,4,5], k = 4, x = 3
    Output: [1,2,3,4]
    Example 2:
    
    Input: arr = [1,2,3,4,5], k = 4, x = -1
    Output: [1,2,3,4]
     
    
    Constraints:
    
    1 <= k <= arr.length
    1 <= arr.length <= 10^4
    Absolute value of elements in the array and x will not exceed 104
    Accepted

--- leetcode/658._Find_K_Closest_Elements.java
+++ leetcode/658._Find_K_Closest_Elements.java
@@ -1,6 +1,67 @@
 
 
 public class Solution {
+   
+   
+   // Clean code
+   
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if(k == 0)  {return list; }
+        
+        int index = binarySearch(arr, x);
+        int left = index;
+        int right;
+               
+        if(left <= 0) { return toList(arr, 0, k); }
+        else if(left == arr.length) { return toList(arr,arr.length - k, arr.length); }       
+        
+        if(left > 0 && arr[left] != x) {
+            if((x - arr[left - 1]) <= (arr[left] - x)) {
+                --left;
+            }
+
+        }
+        right = left + 1;
+        
+        while(right - left <= k) {
+           
+            if(left < 0) {
+                list.add(arr[right++]);
+                continue;
+            }  else if(right == arr.length || x - arr[left] <= arr[right] - x) { 
+                list.addFirst(arr[left--]);
+            }
+            else { 
+                list.add(arr[right++]);
+            }
+        }
+        return list;
+    }
+    
+    public List<Integer> toList(int []arr, int start, int upperBound) {
+        List<Integer> result = new ArrayList<>();
+        for(int index = start; index < upperBound; index++) {
+            result.add(arr[index]);
+        }
+        return result;
+    }
+    
+    public int binarySearch(int []arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int mid;
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            if(arr[mid] < target) { left = mid + 1; }
+            else if(arr[mid] == target){ return mid; }
+            else { right = mid - 1; }
+        }
+        
+        return left;
+    }
+
+   
+   
    // Use of copyOfRange is bit slower than normal usecase 3%
        public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> list = new ArrayList<>();
