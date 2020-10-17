f590608ec9102e9fd3e3dc0bc31a2fb28469fcf9
Jatish Khanna <jatish.khanna@gmail.com>
2020-10-17 10:07:11 +0530

    Update 404._Sum_of_Left_Leaves.java
    Find the sum of all left leaves in a given binary tree.
    
    Example:
    
        3
       / \
      9  20
        /  \
       15   7
    
    There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
    Accepted

--- leetcode/404._Sum_of_Left_Leaves.java
+++ leetcode/404._Sum_of_Left_Leaves.java
@@ -8,6 +8,33 @@
  * }
  */
 class Solution {
+    
+    // with state management
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) { return 0; }
+        return sum(root);
+        // return sum;
+    }
+    
+    private int sum(TreeNode node) {
+        if(node == null) { return 0; }
+        
+        int left = sum(node.left);
+        int right = sum(node.right);
+        
+         if(node.left != null) {
+            if(node.left.left == null && node.left.right == null) {
+                return node.left.val + left + right;
+            }
+        }
+        
+        return left + right;
+    }
+
+    
+    // Without state management
+    
     int sum = 0;
     public int sumOfLeftLeaves(TreeNode root) {
         if(root == null) { return 0; }
