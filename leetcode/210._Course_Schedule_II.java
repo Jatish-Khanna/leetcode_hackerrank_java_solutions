0537fdb7b37ced8de324cf0ef8cad47c1c07e0e2
Jatish Khanna <jatish.khanna@gmail.com>
2020-10-22 07:49:10 +0530

    Update 210._Course_Schedule_II.java
    There are a total of n courses you have to take labelled from 0 to n - 1.
    
    Some courses may have prerequisites, for example, if prerequisites[i] = [ai, bi] this means you must take the course bi before the course ai.
    
    Given the total number of courses numCourses and a list of the prerequisite pairs, return the ordering of courses you should take to finish all courses.
    
    If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
    
     
    
    Example 1:
    
    Input: numCourses = 2, prerequisites = [[1,0]]
    Output: [0,1]
    Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
    Example 2:
    
    Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
    Output: [0,2,1,3]
    Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
    So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
    Example 3:
    
    Input: numCourses = 1, prerequisites = []
    Output: [0]
     
    
    Constraints:
    
    1 <= numCourses <= 2000
    0 <= prerequisites.length <= numCourses * (numCourses - 1)
    prerequisites[i].length == 2
    0 <= ai, bi < numCourses
    ai != bi
    All the pairs [ai, bi] are distinct.
    Accepted

--- leetcode/210._Course_Schedule_II.java
+++ leetcode/210._Course_Schedule_II.java
@@ -1,6 +1,48 @@
 
 
 class Solution {
+    
+    // BFS 
+    
+    static int rIndex = 0;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer> []graph = new ArrayList[numCourses];
+        rIndex = 0;
+        for(int index = 0; index < numCourses; index++) {
+            graph[index] = new ArrayList<>();
+        }
+        int[] indegree = new int[numCourses];
+        for(int []dep : prerequisites) {
+            graph[dep[0]].add(dep[1]);
+            indegree[dep[1]]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int index = 0; index < numCourses; index++) {
+            if(indegree[index] == 0) {
+                queue.offer(index);
+            }
+        }
+        int[] result = new int[numCourses];
+        int rIndex = numCourses - 1;
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            result[rIndex--] = current;
+            for(int neb : graph[current]) {
+                if(--indegree[neb] == 0) {
+                    queue.offer(neb);
+                }
+            }
+        }
+        if(rIndex == -1) {
+            return result;
+        }
+        return new int[]{};
+    }
+
+    
+    // DFS 
+    
     static int rIndex = 0;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<Integer> []graph = new ArrayList[numCourses];
